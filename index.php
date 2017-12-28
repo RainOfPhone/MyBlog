0a7d9ad2317167ffbbb01721eaace20678b67c30
RainOfPhone <792209198@qq.com>
2017-12-29 01:58:59 +0800

    删除多余的样式表
    Signed-off-by: RainOfPhone <792209198@qq.com>

--- index.php
+++ index.php
@@ -34,90 +34,9 @@
 			<div class="context">
 				<div class="l_context">
 					<section>
-						<div class="time">
-							<p>2017-12-27 10:31:33
-						</div>
-						<div class="comment">
-							评论<br />0条
-						</div>
 						<div class="article">
 							<h4>技术产品</h4>
-							<img src="img/thumbs_1.png" />
-							<p>每当产品经理提出一个功能时，我总是会考虑到底该不该去实现它？如何评估功能的开发成本与产品上线
-								后的成效才是一个项目中最困难的部分。以前创业的时候产品是我自己说了算，很容易决定一个功能到底
-								要不要做。但在一个更大的团队中，很多时候技术与产品是有冲突的。</p>
-						</div>
-					</section>
-					<section>
-						<div class="time">
-							2015<br /> 12&nbsp;24
-						</div>
-						<div class="comment">
-							评论<br />0条
-						</div>
-						<div class="article">
-							<h4>技术产品</h4>
-							<img src="img/thumbs_2.jpg"/>
-							<p>每当产品经理提出一个功能时，我总是会考虑到底该不该去实现它？如何评估功能的开发成本与产品上线
-								后的成效才是一个项目中最困难的部分。以前创业的时候产品是我自己说了算，很容易决定一个功能到底
-								要不要做。但在一个更大的团队中，很多时候技术与产品是有冲突的。</p>
-						</div>
-					</section>
-					<section>
-						<div class="time">
-							2015<br /> 12&nbsp;24
-						</div>
-						<div class="comment">
-							评论<br />0条
-						</div>
-						<div class="article">
-							<h4>技术产品</h4>
-							<img src="img/thumbs_3.jpg" />
-							<p>每当产品经理提出一个功能时，我总是会考虑到底该不该去实现它？如何评估功能的开发成本与产品上线
-								后的成效才是一个项目中最困难的部分。以前创业的时候产品是我自己说了算，很容易决定一个功能到底
-								要不要做。但在一个更大的团队中，很多时候技术与产品是有冲突的。</p>
-						</div>
-					</section>
-					<section>
-						<div class="time">
-							2015<br /> 12&nbsp;24
-						</div>
-						<div class="comment">
-							评论<br />0条
-						</div>
-						<div class="article">
-							<h4>技术产品</h4>
-							<img src="img/thumbs_4.jpg" />
-							<p>每当产品经理提出一个功能时，我总是会考虑到底该不该去实现它？如何评估功能的开发成本与产品上线
-								后的成效才是一个项目中最困难的部分。以前创业的时候产品是我自己说了算，很容易决定一个功能到底
-								要不要做。但在一个更大的团队中，很多时候技术与产品是有冲突的。</p>
-						</div>
-					</section>
-					<section>
-						<div class="time">
-							2015<br /> 12&nbsp;24
-						</div>
-						<div class="comment">
-							评论<br />0条
-						</div>
-						<div class="article">
-							<h4>技术产品</h4>
-							<img src="img/thumbs_5.jpg" />
-							<p>每当产品经理提出一个功能时，我总是会考虑到底该不该去实现它？如何评估功能的开发成本与产品上线
-								后的成效才是一个项目中最困难的部分。以前创业的时候产品是我自己说了算，很容易决定一个功能到底
-								要不要做。但在一个更大的团队中，很多时候技术与产品是有冲突的。</p>
-						</div>
-					</section>
-					<section>
-						<div class="time">
-							2015<br /> 12&nbsp;24
-						</div>
-						<div class="comment">
-							评论<br />0条
-						</div>
-						<div class="article">
-							<h4>技术产品</h4>
-							<img src="img/thumbs_6.jpg" />
+                            <h5>时间：<span>2015-12-27 11:30:33</span></h5>
 							<p>每当产品经理提出一个功能时，我总是会考虑到底该不该去实现它？如何评估功能的开发成本与产品上线
 								后的成效才是一个项目中最困难的部分。以前创业的时候产品是我自己说了算，很容易决定一个功能到底
 								要不要做。但在一个更大的团队中，很多时候技术与产品是有冲突的。</p>
@@ -127,26 +46,8 @@
 				</div>
 				<div class="r_context">
 					<div>
-						<input type="text" placeholder="search" />搜
-					</div>
-					<div>
-						<h3>神奇海螺</h3>
-					</div>
-					<div>
-						<h3>最新评论</h3>
-						<input type="text" />
-						<input type="text" />
-						<input type="text" />
-						<input type="text" />
-						<input type="text" />
-					</div>
-					<div>
-						<h3>海螺消息</h3>
-						<input type="text" />
-						<input type="text" />
-						<input type="text" />
-						<input type="text" />
-						<input type="text" />
+						<input type="text" placeholder="search" />
+                        <input type="submit" value="搜索" />
 					</div>
 				</div>
 			</div>
